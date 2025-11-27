7cd1ef8abc1ca54ed7cc5756732f1bde604d4517
hexagonbot932 <naninithin687@gmail.com>
2025-11-27 10:31:08 +0530

    Update mock.js

--- src/mock.js
+++ src/mock.js
@@ -7,7 +7,7 @@ export const portfolioData = {
     email: "HELLO@COZY.COM",
     yearsExperience: 11,
     availableForWork: true,
-    image: "https://unsplash.com/photos/man-in-white-crew-neck-shirt-wearing-black-framed-eyeglasses-C8Ta0gwPbQg",
+    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     mission: "I design meaningful products that create positive impact and deliver exceptional user experiences."
   },
 
